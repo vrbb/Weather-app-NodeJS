8a1e7e1f203607a9180f3d47fe524f34370f2b41
vrbb <victoriaricartebispo@gmail.com>
2022-05-30 10:54:22 -0300

    call stack, callback and event loop

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ setTimeout(() => {
   console.log('2 Second Timer')
 }, 2000)
 
+
+setTimeout(() =>{
+  console.log('0 second Timer')
+}, 0)
+
+const x = 5
+
+const y = x + 2
+
+console.log('Sum is '+y)
+
 console.log('Stopping')
