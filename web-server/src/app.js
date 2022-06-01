ece70d5e2da8bc58e09791ec37ee7b4c4b8f2e4e
vrbb <victoriaricartebispo@gmail.com>
2022-06-01 13:49:05 -0300

    Serving up HTML ans JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,42 @@
 const express = require('express')
+const geocode = require('/Users/vntvibe/Documents/Projetos de estudo/Weather-app-NodeJS/utils/geocode.js')
+const forecast = require('/Users/vntvibe/Documents/Projetos de estudo/Weather-app-NodeJS/utils/forecast.js')
 
 const app = express()
+const address = process.argv[2]
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Hello express!</h1>')
 })
 
 app.get('/help', (req,res) => {
-    res.send('Help page')
+    res.send('<h1>Help page</h1>')
 })
 
 app.get('/about', (req,res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather',(req, res) => {
-    res.send('Weather page')
+    
+        if (!address){
+            res.send({ 'Error': 'please, provide an address'})
+        } else {
+            
+            geocode.geocode(address, (error, {latitude, longitude, location} = {} ) => {
+              if (error) {
+                res.send({ 'Error': error})
+              } 
+                
+              forecast.forecast(location, (error, data) => {
+                if (error) {
+                  res.send({ 'Error': error})
+                } 
+                res.send({ 'Data': data})
+              })
+            })    
+        }
+    
 })
 
 // app.com
