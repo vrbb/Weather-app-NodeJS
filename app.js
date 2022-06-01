e3e6130d42bc016b781ca7e9ab1fba093752dfe1
vrbb <victoriaricartebispo@gmail.com>
2022-06-01 08:24:50 -0300

    CallBack Chaining

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const request = require('request')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
+const yargs = require('yargs')
+
+const address = process.argv[2]
+
+if (!address){
+  console.log('please, provide an address')
+} else {
+  console.log(process.argv)
+  
+  geocode.geocode(address, (error, data) => {
+    if (error) {
+      return console.log('Error', error)
+    } 
+      
+    forecast.forecast(data.location, (error, data) => {
+      if (error) {
+        return console.log('Error', error)
+      } 
+      console.log('Data', data)
+    })
+  })    
+}
 
 
+yargs.parse()
 
-const geoData = geocode.geocode('Philadelphia', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-  return data
-})
-
-
-forecast.forecast('Philadelphia', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
