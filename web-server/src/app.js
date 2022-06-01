7853cdf4b783408ecef3afcdf110333e908a287f
vrbb <victoriaricartebispo@gmail.com>
2022-06-01 15:17:24 -0300

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,9 +8,26 @@ const publicDirectoryPath =  path.join(__dirname, '../public')
 
 
 const app = express()
+
 const address = process.argv[2]
 
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name : 'this is a name'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title: 'About me',
+        name : 'Victoria Bispo'
+    })
+})
 
 app.get('/weather',(req, res) => {
     
@@ -38,6 +55,6 @@ app.get('/weather',(req, res) => {
 // app.com/help
 // app.com/about
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(3131, () => {
+    console.log('Server is up on port 3131.')
 })
