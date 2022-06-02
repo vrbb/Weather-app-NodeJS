db0f3823874a947660bff966e75a5b159c326b10
vrbb <victoriaricartebispo@gmail.com>
2022-06-02 13:15:54 -0300

    advNCED TEMPLATIONS

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,23 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
+const app = express()
+
+// Define paths for Express config
 const publicDirectoryPath =  path.join(__dirname, '../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 // const htmlPage = require(dirName+'/index.html')
 // const geocode = require(dirName+'/utils/geocode.js')
 // const forecast = require(dirName+'/utils/forecast.js')
 
-
-const app = express()
-
-const address = process.argv[2]
-
+// Stuup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res) => {
